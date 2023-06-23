9f9d4ab7bb0236e62914b1841dfb01b63761ccf4
ignacio <ignacio.mustafha@4rsoluciones.com>
2023-06-23 12:32:54 -0300

    Cors config

--- SRC/server.js
+++ SRC/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import bodyParser from "body-parser";
 import cardController from "./Card/Controller/index.js";
+import cors from "cors";
 
 const app = express();
 const port = 3001;
@@ -12,6 +13,16 @@ app.use(
   })
 );
 app.use(cardController);
+const allowedOrigins = [
+  "http://localhost:3001",
+  "https://to-do-list-classes-production.up.railway.app/",
+];
+const corsOptions = {
+  origin: allowedOrigins,
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
 app.listen(port, () => {
   console.log("The server is running");
 });
