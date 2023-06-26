7f2c380e3b4081e482020f861ff957910fbf6e08
ignacio <ignacio.mustafha@4rsoluciones.com>
2023-06-26 12:40:31 -0300

    back to *

--- SRC/server.js
+++ SRC/server.js
@@ -6,12 +6,12 @@ import cors from "cors";
 const app = express();
 const port = process.env.PORT || 8000;
 
-const corsOptions = {
-  origin: "https://to-do-list-classes-production.up.railway.app",
-  credentials: true,
-  optionSuccessStatus: 200,
-};
-app.use(cors(corsOptions));
+// const corsOptions = {
+//   origin: "https://to-do-list-classes-production.up.railway.app",
+//   credentials: true,
+//   optionSuccessStatus: 200,
+// };
+app.use(cors("*"));
 app.use(bodyParser.json());
 app.use(
   bodyParser.urlencoded({
